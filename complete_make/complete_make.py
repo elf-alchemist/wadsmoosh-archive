1f942d91b9801d2bb3e383cc189c72ee452183b8
jplebreton <unknown>
2016-07-03 14:41:31 -0700

    - start of script for compiling "doom complete" pk3

---
+++ complete_make/complete_make.py
@@ -0,0 +1,65 @@
+
+import os
+import omg
+
+SRC_WAD_DIR = 'source_wads/'
+RES_DIR = 'res/'
+MAPS_DIR = RES_DIR + 'maps/'
+WAD_EXT = 'wad'
+
+IWADS = ['doom', 'doom2', 'tnt', 'plutonia', 'nerve']
+
+# prefixes for filenames of maps extracted from IWADs
+WAD_MAP_PREFIXES = {
+    'doom': '',
+    'doom2': '',
+    'tnt': 'tnt_',
+    'plutonia': 'plut_',
+    'nerve': 'nerve_'
+}
+
+# lists of lumps to extract from each IWAD
+WAD_LUMP_LISTS = {
+    'doom': 'doom1_lumps.txt',
+    'doom2': 'doom2_lumps.txt',
+    'tnt': 'tnt_lumps.txt',
+    'plutonia': 'plutonia_lumps.txt'
+}
+
+CORE_LUMPS = [
+    'playpal', 'colormap', 'endoom', 'genmidi', 'dmxgus'
+]
+
+MASTER_LEVELS_MAP_ORDER = [
+    'attack', 'canyon', 'catwalk', 'combine', 'fistula', 'garrison', 'manor',
+    'paradox', 'subspace', 'subterra', 'ttrap', 'virgil', 'minos', 'bloodsea',
+    'mephisto', 'nessus', 'geryon', 'vesperas', 'blacktwr', 'teeth'
+]
+
+MASTER_LEVELS_MAP_PREFIX = 'ml_'
+
+def get_iwad_path(base_name):
+    return SRC_WAD_DIR + base_name + '.' + WAD_EXT
+
+def extract_map(in_wad, map_name, out_filename):
+    out_wad = omg.WAD()
+    ed = omg.MapEditor(in_wad.maps[map_name])
+    out_wad.maps[map_name] = ed.to_lumps()
+    out_wad.to_file(out_filename)
+
+def extract_iwad_maps(wad_name, map_prefix):
+    in_wad_filename = get_iwad_path(wad_name)
+    if not os.path.exists(in_wad_filename):
+        print('IWAD %s not found' % in_wad_filename)
+        return
+    in_wad = omg.WAD()
+    in_wad.from_file(in_wad_filename)
+    for map_name in in_wad.maps.find('*'):
+        print('  processing map %s...' % map_name)
+        out_wad_filename = MAPS_DIR + map_prefix + map_name + '.' + WAD_EXT
+        extract_map(in_wad, map_name, out_wad_filename)
+        print('  saved map %s' % out_wad_filename)
+
+for iwad_name in IWADS:
+    print('processing IWAD %s...' % iwad_name)
+    extract_iwad_maps(iwad_name, WAD_MAP_PREFIXES[iwad_name])
