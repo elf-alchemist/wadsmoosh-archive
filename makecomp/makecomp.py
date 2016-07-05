47fcebed1624c490f12d7b20c189e06f3c626e61
jplebreton <unknown>
2016-07-05 14:53:50 -0700

    - split doom1 and doom2 textures into separate files, don't copy if wad isn't present - log to a file - mac/linux run scripts - source distribution build script

--- makecomp/makecomp.py
+++ makecomp/makecomp.py
@@ -7,21 +7,24 @@ import omg
 
 """
 TODO:
-- texture error in tnt map20: "Unknown middle texture 'BLOD3' on first side of linedef 125"
 - split doom1 and doom2 textures into separate files, don't copy if wad isn't present
+- log to a file
+- mac/linux run scripts
+- source distribution build script
 - finish custom ENDOOM art
-- test with various wads missing
 - win32 exe build
 """
 
 SRC_WAD_DIR = 'source_wads/'
 DATA_DIR = 'data/'
 DEST_DIR = 'pk3/'
-DEST_FILENAME = 'complete.pk3'
+DEST_FILENAME = 'doom_complete.pk3'
+LOG_FILENAME = 'makecomp.log'
 RES_DIR = 'res/'
 RES_FILES = [
     'mapinfo.txt', 'language.txt', 'endoom',
-    'textures.txt', 'textures.tnt', 'textures.plut',
+    'textures.doom1a', 'textures.doom1b', 'textures.doom2',
+    'textures.tnt', 'textures.plut',
     'graphics/M_DOOM.png', 'graphics/TITLEPIC.png',
     'mapinfo/doom1_levels.txt', 'mapinfo/doom2_levels.txt',
     'mapinfo/masterlevels.txt', 'mapinfo/tnt_levels.txt',
@@ -75,6 +78,15 @@ MASTER_LEVELS_PATCHES = {
     'virgil': ('RSKY1', 'ML_SKY3')
 }
 
+logfile = None
+
+def logg(line):
+    global logfile
+    if not logfile:
+        logfile = open(LOG_FILENAME, 'w')
+    print(line)
+    logfile.write(line + '\n')
+
 def get_wad_filename(wad_name):
     # return filename of first case-insensitive match
     wad_name += '.wad'
@@ -87,9 +99,9 @@ def extract_master_levels():
     # check if present first
     first_ml_wad = get_wad_filename(MASTER_LEVELS_MAP_ORDER[0])
     if not first_ml_wad:
-        print('Master Levels not found.')
+        logg('Master Levels not found.')
         return
-    print('Processing Master Levels...')
+    logg('Processing Master Levels...')
     for i,wad_name in enumerate(MASTER_LEVELS_MAP_ORDER):
         in_wad = omg.WAD()
         wad_filename = get_wad_filename(wad_name)
@@ -97,14 +109,14 @@ def extract_master_levels():
         out_wad_filename = DEST_DIR + 'maps/' + MASTER_LEVELS_MAP_PREFIX + 'map'
         # extra zero for <10 map numbers, eg map01
         out_wad_filename += str(i + 1).rjust(2, '0') + '.wad'
-        print('  Writing %s to %s' % (wad_filename, out_wad_filename))
+        logg('  Extracting %s to %s' % (wad_filename, out_wad_filename))
         # grab first map we find in each wad
         map_name = in_wad.maps.find('*')[0]
         ed = omg.MapEditor(in_wad.maps[map_name])
         extract_map(in_wad, map_name, out_wad_filename)
     # save teeth map32 to map21 wad
     out_wad_filename = DEST_DIR + 'maps/' + MASTER_LEVELS_MAP_PREFIX + 'map21' + '.wad'
-    print('  Writing %s map32 to %s' % (wad_filename, out_wad_filename))
+    logg('  Extracting %s map32 to %s' % (wad_filename, out_wad_filename))
     extract_map(in_wad, in_wad.maps.find('*')[1], out_wad_filename)
     # extract sky lumps
     for wad_name,patch_replace in MASTER_LEVELS_PATCHES.items():
@@ -117,13 +129,13 @@ def extract_master_levels():
         else:
             lump = wad.patches[patch_replace[0]]
         out_filename = DEST_DIR + 'patches/' + patch_replace[1] + '.lmp'
-        print('  Writing %s lump from %s as %s' % (patch_replace[0],
+        logg('  Extracting %s lump from %s as %s' % (patch_replace[0],
                                                    wad_filename,
                                                    patch_replace[1]))
         lump.to_file(out_filename)
 
 def tnt_map31_fix():
-    print('Checking for TNT MAP31 fix...')
+    logg('Checking for TNT MAP31 fix...')
     wad = omg.WAD()
     wad_filename = DEST_DIR + 'maps/' + WAD_MAP_PREFIXES['tnt'] + 'MAP31.wad'
     wad.from_file(wad_filename)
@@ -131,12 +143,12 @@ def tnt_map31_fix():
     yellow_key = maped.things[470]
     if yellow_key.type != 6 or \
        (yellow_key.type == 6 and not yellow_key.get_multiplayer()):
-        print('  TNT MAP31 already fixed.')
+        logg('  TNT MAP31 already fixed.')
         return
     yellow_key.set_multiplayer(False)
     wad.maps['MAP31'] = maped.to_lumps()
     wad.to_file(wad_filename)
-    print('  TNT MAP31 fix applied.')
+    logg('  TNT MAP31 fix applied.')
 
 def extract_map(in_wad, map_name, out_filename):
     out_wad = omg.WAD()
@@ -149,10 +161,10 @@ def extract_iwad_maps(wad_name, map_prefix):
     wad_filename = get_wad_filename(wad_name)
     in_wad.from_file(wad_filename)
     for map_name in in_wad.maps.find('*'):
-        print('  Processing map %s...' % map_name)
+        logg('  Extracting map %s...' % map_name)
         out_wad_filename = DEST_DIR + 'maps/' + map_prefix + map_name + '.wad'
         extract_map(in_wad, map_name, out_wad_filename)
-        print('  Saved map %s' % out_wad_filename)
+        #logg('  Saved map %s' % out_wad_filename)
 
 def extract_lumps(wad_name):
     if not wad_name in WAD_LUMP_LISTS:
@@ -165,13 +177,13 @@ def extract_lumps(wad_name):
         try:
             lump_type = lump_list[:lump_list.index('_')]
         except ValueError:
-            print("Couldn't identify type of lump list %s" % lump_list)
+            logg("Couldn't identify type of lump list %s" % lump_list)
             continue
         lump_table = getattr(wad, lump_type, None)
         if not lump_table:
-            print('  Lump type %s not found' % lump_type)
+            logg('  Lump type %s not found' % lump_type)
             continue
-        print('  extracting %s...' % lump_list)
+        logg('  extracting %s...' % lump_list)
         # write PLAYPAL etc to pk3 root
         if lump_type == 'data':
             lump_subdir = DEST_DIR
@@ -194,11 +206,11 @@ def extract_lumps(wad_name):
                 lump_name = lump_name.strip()
                 out_filename = out_filename.strip()
             if not lump_name in lump_table:
-                print("  Couldn't find lump with name %s" % lump_name)
+                logg("  Couldn't find lump with name %s" % lump_name)
                 continue
             lump = lump_table[lump_name]
             out_filename += '.lmp' if lump_type != 'music' else '.mus'
-            print('    Writing %s' % lump_subdir + out_filename)
+            logg('    Extracting %s' % lump_subdir + out_filename)
             lump.to_file(lump_subdir + out_filename)
 
 def main():
@@ -213,31 +225,35 @@ def main():
     for iwad_name in IWADS:
         wad_filename = get_wad_filename(iwad_name)
         if not wad_filename:
-            print('IWAD %s not found' % iwad_name)
+            logg('IWAD %s not found' % iwad_name)
             continue
         if iwad_name == 'nerve' and not get_wad_filename('doom2'):
-            print('Skipping nerve.wad as doom2.wad is not present')
+            logg('Skipping nerve.wad as doom2.wad is not present')
             continue
-        print('Processing IWAD %s...' % iwad_name)
+        logg('Processing IWAD %s...' % iwad_name)
         extract_lumps(iwad_name)
         extract_iwad_maps(iwad_name, WAD_MAP_PREFIXES[iwad_name])
     if get_wad_filename('doom2'):
         extract_master_levels()
     else:
-        print('Skipping Master Levels as doom2.wad is not present')
+        logg('Skipping Master Levels as doom2.wad is not present')
     if get_wad_filename('tnt'):
         tnt_map31_fix()
     # copy pre-authored lumps eg mapinfo
     for src_file in RES_FILES:
         # don't copy texture lumps for files that aren't present
-        if src_file == 'textures.tnt' and not get_wad_filename('tnt'):
+        if src_file.startswith('textures.doom1') and not get_wad_filename('doom'):
+            continue
+        elif src_file == 'textures.doom2' and not get_wad_filename('doom2'):
+            continue
+        elif src_file == 'textures.tnt' and not get_wad_filename('tnt'):
             continue
         elif src_file == 'textures.plut' and not get_wad_filename('plutonia'):
             continue
-        print('Copying %s' % src_file)
+        logg('Copying %s' % src_file)
         copyfile(RES_DIR + src_file, DEST_DIR + src_file)
     # create pk3
-    print('Creating %s...' % DEST_FILENAME)
+    logg('Creating %s...' % DEST_FILENAME)
     pk3 = ZipFile(DEST_FILENAME, 'w')
     for dir_name, x, filenames in os.walk(DEST_DIR):
         for filename in filenames:
@@ -246,7 +262,8 @@ def main():
             dest_name = src_name[len(DEST_DIR):]
             pk3.write(src_name, dest_name)
     pk3.close()
-    print('Done!')
+    logg('Done!')
+    logfile.close()
 
 if __name__ == "__main__":
     main()
