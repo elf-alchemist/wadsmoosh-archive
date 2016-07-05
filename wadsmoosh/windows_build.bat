180fa965baf8e7e73800a275cc08cd8eb959924b
jplebreton <unknown>
2016-07-05 16:12:21 -0700

    include readme in windows build zip

--- wadsmoosh/windows_build.bat
+++ wadsmoosh/windows_build.bat
@@ -7,4 +7,5 @@ pause
 xcopy /E data\*.* dist\%PROJ_NAME%\data\
 xcopy /E res\*.* dist\%PROJ_NAME%\res\
 xcopy /E source_wads\delete_me.txt dist\%PROJ_NAME%\source_wads\
-%ZIP_EXE% a -r %PROJ_NAME%_win.zip ./dist/%PROJ_NAME%/*
+xcopy wadsmoosh.txt dist\%PROJ_NAME%\
+%ZIP_EXE% a -r %PROJ_NAME%_win.zip .\dist\%PROJ_NAME%\*
